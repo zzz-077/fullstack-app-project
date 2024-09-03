7de5a222b1b2b98ea9d7eda4faa5da0fbf965031
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-09-04 00:05:27 +0400

    created socketio setup

--- Backend/index.js
+++ Backend/index.js
@@ -6,21 +6,32 @@ import dotenv from "dotenv";
 import router from "./src/routes/router.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import "./auth.js";
+import handleSocketConnection from "./src/services/socketIoService.js";
 dotenv.config();
 const app = express();
-app.use(express.text());
+//socketIo
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: `http://localhost:${process.env.FRONT_PORT}`,
+    },
+});
+io.on("connection", (socket) => handleSocketConnection(io, socket));
 
 // Enable CORS
 app.use(
-  cors({
-    origin: `http://localhost:${process.env.FRONT_PORT}`,
-    credentials: true,
-    methods: "GET,POST,PUT,DELETE",
-  })
+    cors({
+        origin: `http://localhost:${process.env.FRONT_PORT}`,
+        credentials: true,
+        methods: "GET,POST,PUT,DELETE",
+    })
 );
 // Middleware to parse JSON
 app.use(express.json());
+app.use(express.text());
 //coockieParser
 app.use(cookieParser());
 app.use(passport.initialize());
@@ -28,13 +39,13 @@ app.use(passport.initialize());
 app.use("/", router);
 //connecting to database
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected to database saccessfully!");
-    app.listen(process.env.PORT, (port) => {
-      return console.log("Listening on port:", process.env.PORT + "!");
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to database saccessfully!");
+        app.listen(process.env.PORT, (port) => {
+            return console.log("Listening on port:", process.env.PORT + "!");
+        });
+    })
+    .catch((error) => {
+        console.log("Error occurred while connecting to database", error);
     });
-  })
-  .catch((error) => {
-    console.log("Error occurred while connecting to database", error);
-  });
---
+++ Backend/src/services/socketIoService.js
@@ -0,0 +1,4 @@
+function handleSocketConnection(io, socket) {
+    console.log("socketIo works on socket:", socket);
+}
+export default handleSocketConnection;
