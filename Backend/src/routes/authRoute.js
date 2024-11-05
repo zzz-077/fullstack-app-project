041579c48ddaf246fe5648a94d8c909707eba468
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-11-05 23:20:26 +0400

    updated

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -1,48 +1,48 @@
 import express from "express";
 import passport from "passport";
 import {
-    createAccessToken,
-    createRefreshToken,
+  createAccessToken,
+  createRefreshToken,
 } from "../utils/createAccessToken.js";
 import {
-    createAccessTokenCookie,
-    createRefreshTokenCookie,
+  createAccessTokenCookie,
+  createRefreshTokenCookie,
 } from "../utils/cookiesUtils.js";
 const router = express.Router();
 
 router.get(
-    "/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email"],
-        session: false,
-    })
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    session: false,
+  })
 );
 router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: `http://localhost:${process.env.FRONT_PORT}/signin`,
-        session: false,
-    }),
-    async (req, res) => {
-        const AccessToken = await createAccessToken(req.user._id);
-        const RefreshToken = await createRefreshToken(req.user._id);
-        await createAccessTokenCookie(res, AccessToken);
-        await createRefreshTokenCookie(res, RefreshToken);
-        // return res.status(200).json({
-        //     status: "success",
-        //     message: "Authentication success",
-        //     error: null,
-        // });
-        return res.redirect(`http://localhost:${process.env.FRONT_PORT}/home`);
-    },
-    (err, req, res, next) => {
-        // return res.status(500).json({
-        //     status: "fail",
-        //     message: "Authentication failed",
-        //     error: err.message,
-        // });
-        res.redirect(`http://localhost:${process.env.FRONT_PORT}/signin`);
-    }
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `http://localhost:${process.env.FRONT_PORT}/signin`,
+    session: false,
+  }),
+  async (req, res) => {
+    const AccessToken = await createAccessToken(req.user._id);
+    const RefreshToken = await createRefreshToken(req.user._id);
+    await createAccessTokenCookie(res, AccessToken);
+    await createRefreshTokenCookie(res, RefreshToken);
+    // return res.status(200).json({
+    //     status: "success",
+    //     message: "Authentication success",
+    //     error: null,
+    // });
+    return res.redirect(`http://localhost:${process.env.FRONT_PORT}/home`);
+  },
+  (err, req, res, next) => {
+    // return res.status(500).json({
+    //     status: "fail",
+    //     message: "Authentication failed",
+    //     error: err.message,
+    // });
+    res.redirect(`http://localhost:${process.env.FRONT_PORT}/signin`);
+  }
 );
 
 export default router;
