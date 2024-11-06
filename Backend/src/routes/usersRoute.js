9cbb8b41bae9ab986f097c237f5ca2c7a049de40
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-11-06 19:26:26 +0400

    fixed import fonts from googlefont and yellow underlined errors

--- Backend/src/routes/usersRoute.js
+++ Backend/src/routes/usersRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import usersGet from "../services/usersGetService.js";
+// import usersGet from "../services/usersGetService.js";
 import userCreate from "../services/userCreateService.js";
 import signIn from "../services/signInService.js";
 const router = express.Router();
