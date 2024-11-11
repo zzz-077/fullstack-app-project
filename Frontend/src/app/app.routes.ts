6cc22c53b8330672515d8913684566aed82fdafa
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-11-11 16:32:15 +0400

    changed app.routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { RegistrationComponent } from './components/pages/signup/signup.componen
 import { SigninComponent } from './components/pages/signin/signin.component';
 import { UndefinedPageComponent } from './components/pages/undefined-page/undefinedPage.component';
 import { MainComponent } from './components/pages/main/main.component';
-import { RegistrationService } from './shared/services/registrationService/registration.service';
 import { LogoutComponent } from './components/tools/logout/logout.component';
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: MainComponent },
   { path: 'signup', component: RegistrationComponent },
   { path: 'signin', component: SigninComponent },
