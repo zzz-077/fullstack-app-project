fd0270fb35f8645fbb2126a31af05aafcc0dff2a
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-11-11 16:05:49 +0400

    changed vercel.json & index.js front url added '/' at the end

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
     // origin: `https://localhost:${process.env.FRONT_PORT}`,
-    origin: `https://chatz-prj.vercel.app`,
+    origin: `https://chatz-prj.vercel.app/`,
     credentials: true,
   },
 });
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
 app.use(
   cors({
     // origin: `https://localhost:${process.env.FRONT_PORT}`,
-    origin: `https://chatz-prj.vercel.app`,
+    origin: `https://chatz-prj.vercel.app/`,
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
   })
