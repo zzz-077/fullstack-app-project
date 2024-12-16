4a419f22008f09fe7b0bc198299c3bd4a4644a58
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-12-16 11:40:19 +0400

    added domain in cookies utils

--- Backend/src/utils/cookiesUtils.js
+++ Backend/src/utils/cookiesUtils.js
@@ -1,20 +1,22 @@
 async function createAccessTokenCookie(res, AccessToken) {
-    return res.cookie("AccessToken", AccessToken, {
-        httpOnly: true,
-        sameSite: "None",
-        secure: true,
-        path: "/",
-        maxAge: 1 * 3600 * 1000,
-    });
+  return res.cookie("AccessToken", AccessToken, {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    path: "/",
+    domain: "chatz-prj.vercel.app",
+    maxAge: 1 * 3600 * 1000,
+  });
 }
 async function createRefreshTokenCookie(res, RefreshToken) {
-    return res.cookie("RefreshToken", RefreshToken, {
-        httpOnly: true,
-        sameSite: "None",
-        secure: true,
-        path: "/",
-        maxAge: 24 * 3600 * 1000,
-    });
+  return res.cookie("RefreshToken", RefreshToken, {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    path: "/",
+    domain: "chatz-prj.vercel.app",
+    maxAge: 24 * 3600 * 1000,
+  });
 }
 
 export { createAccessTokenCookie, createRefreshTokenCookie };
